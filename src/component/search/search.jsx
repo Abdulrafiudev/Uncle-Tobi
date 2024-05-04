4d4b714dba0098325899a1b48a232f338603dbcf
Abdulrafiu-Python <abolajiibrahim70@gmail.com>
2024-05-04 10:30:09 +0100

    new commit

--- src/component/search/search.jsx
+++ src/component/search/search.jsx
@@ -1,5 +1,8 @@
 import React from "react"
 import "./search.css"
+import { UilShoppingCart } from "@iconscout/react-unicons"
+import { UilEnvelope } from "@iconscout/react-unicons"
+import { UilPhone } from "@iconscout/react-unicons"
 
 function Search() {
   return (
@@ -12,7 +15,22 @@ function Search() {
             className="search_input"
           />
         </div>
-        <div className="right_search"></div>
+        <div className="right_search">
+          <div className="cart_cointainer">
+            <UilShoppingCart />
+            <div className="quantity_container">
+              <span className="cart_quantity"> 0 </span>
+            </div>
+          </div>
+          <div className="mail_cointainer">
+            <UilEnvelope />
+            <span className="mail_span"> support@uncletobi.shop </span>
+          </div>
+          <div className="phone_cointainer">
+            <UilPhone />
+            <span className="telephone_span"> +44 278 910 9301 </span>
+          </div>
+        </div>
       </div>
     </>
   )
