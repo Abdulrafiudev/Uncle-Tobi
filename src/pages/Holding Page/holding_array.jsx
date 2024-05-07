4332d945bee5868edda6b2b5fc80ca34f0b354f8
Abdulrafiu-Python <abolajiibrahim70@gmail.com>
2024-05-07 04:01:58 +0100

    little changes

--- src/pages/Holding Page/holding_array.jsx
+++ src/pages/Holding Page/holding_array.jsx
@@ -1,43 +1,41 @@
 import React from "react"
 import first_logo from "../../assets/images/first_logo.png"
 import second_logo from "../../assets/images/second_logo.png"
-import third_logo from "../../assets/images/third_logo.jpg"
-import fourth_logo from "../../assets/images/fourth_logo.jpg"
+import third_logo from "../../assets/images/third_logo.png"
+import fourth_logo from "../../assets/images/fourth_logo.png"
 import fifth_logo from "../../assets/images/fifth_logo.png"
-import sixth_logo from "../../assets/images/sixth_logo.jpg"
+import sixth_logo from "../../assets/images/sixth_logo.png"
 
 let Logo = [
   {
     img: first_logo,
     height: "70px",
     width: "70px",
-    margin_right: "20px",
   },
   {
     img: second_logo,
-    height: "80px",
-    width: "80px",
+    height: "70px",
+    width: "70px",
   },
   {
     img: third_logo,
-    height: "150px",
-    width: "150px",
+    height: "70px",
+    width: "70px",
   },
   {
     img: fourth_logo,
-    height: "180px",
-    width: "180px",
-    margin_left: "-80px",
+    height: "90px",
+    width: "90px",
   },
   {
     img: fifth_logo,
-    height: "20px",
-    width: "130px",
+    height: "50px",
+    width: "150px",
   },
   {
     img: sixth_logo,
-    height: "150px",
-    width: "150px",
+    height: "80px",
+    width: "75px",
   },
 ]
 
